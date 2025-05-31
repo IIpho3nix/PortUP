8745e57d23aad6c3b0c629a8a78c9901b733f3a8
IIpho3nix <79083095+IIpho3nix@users.noreply.github.com>
2025-05-31 19:48:04 +0300

    add clean command, add more error and edge-case handling

--- PortUP.go
+++ PortUP.go
@@ -28,6 +28,11 @@ type Mapping struct {
 	Protocol   string
 }
 
+var logger = log.NewWithOptions(os.Stderr, log.Options{
+	ReportTimestamp: true,
+	TimeFormat:      time.Kitchen,
+})
+
 func parseArgs(args []string, protocol string) ([]Mapping, error) {
 	var mappings []Mapping
 	for _, arg := range args {
@@ -80,6 +85,23 @@ func parseArgs(args []string, protocol string) ([]Mapping, error) {
 		default:
 			return nil, fmt.Errorf("invalid format: %s", arg)
 		}
+
+		if !isValidPort(localPort) {
+			return nil, fmt.Errorf("invalid local port: %d", localPort)
+		}
+		if !isValidPort(remotePort) {
+			return nil, fmt.Errorf("invalid remote port: %d", remotePort)
+		}
+
+		if !isValidLocalIP(localIP) {
+			return nil, fmt.Errorf("invalid local IP: %s", localIP)
+		} else {
+			if !isInSameRange(localIP, getLocalIP()) {
+				logger.Warnf("Local IP %s is not in the same network range as the local IP of current machine %s, UPnP may fail.", localIP, getLocalIP())
+				logger.Warn(("Are you sure you entered the correct IP address?"))
+			}
+		}
+
 		mappings = append(mappings, Mapping{
 			LocalPort:  localPort,
 			RemotePort: remotePort,
@@ -100,6 +122,54 @@ func getLocalIP() string {
 	return localAddr.IP.String()
 }
 
+func isValidPort(port int) bool {
+	return port >= 1 && port <= 65535
+}
+
+func isValidLocalIP(ipStr string) bool {
+	ip := net.ParseIP(ipStr)
+	if ip == nil {
+		return false
+	}
+
+	privateCIDRs := []string{
+		"10.0.0.0/8",
+		"172.16.0.0/12",
+		"192.168.0.0/16",
+	}
+
+	for _, cidr := range privateCIDRs {
+		_, subnet, _ := net.ParseCIDR(cidr)
+		if subnet.Contains(ip) {
+			return true
+		}
+	}
+	return false
+}
+
+func isInSameRange(ipStr1, ipStr2 string) bool {
+	ip1 := net.ParseIP(ipStr1)
+	ip2 := net.ParseIP(ipStr2)
+	if ip1 == nil || ip2 == nil {
+		return false
+	}
+
+	privateCIDRs := []string{
+		"10.0.0.0/8",
+		"172.16.0.0/12",
+		"192.168.0.0/16",
+		"127.0.0.0/8",
+	}
+
+	for _, cidr := range privateCIDRs {
+		_, subnet, _ := net.ParseCIDR(cidr)
+		if subnet.Contains(ip1) && subnet.Contains(ip2) {
+			return true
+		}
+	}
+	return false
+}
+
 func printLogo() {
 	logo := `
  ____    ___   ____   ______  __ __  ____
@@ -116,7 +186,7 @@ l__j    \___/ l__j\_j  l__j   \__,_jl__j
 
 func printUsage() {
 	fmt.Println(`Usage:
-  PortUP <tcp|udp> <port mapping> [<port mapping> ...]
+  PortUP <tcp|udp|clean protocol > <port mapping> [<port mapping> ...]
 
 Description:
   Forward local ports to remote ports over TCP or UDP.
@@ -132,27 +202,32 @@ Examples:
   PortUP udp 192.168.1.101:5000
   PortUP tcp 192.168.1.101:8080~80
   PortUP udp 8080 192.168.1.50:1234~5678
+  PortUP clean tcp 8080 80 81 
+  PortUP clean udp 80
   `)
 }
 
 func main() {
-	logger := log.NewWithOptions(os.Stderr, log.Options{
-		ReportTimestamp: true,
-		TimeFormat:      time.Kitchen,
-	})
-
 	if len(os.Args) < 3 {
 		printUsage()
 		os.Exit(1)
 	}
 
 	protocol := strings.ToLower(os.Args[1])
-	if protocol != "tcp" && protocol != "udp" {
+	if protocol != "clean" && protocol != "tcp" && protocol != "udp" {
 		printUsage()
 		logger.Fatalf("Invalid protocol: %s. Must be tcp or udp.", protocol)
 	}
 
-	mappings, err := parseArgs(os.Args[2:], protocol)
+	var mappings []Mapping
+	var err error
+
+	if protocol == "clean" {
+		mappings, err = parseArgs(os.Args[3:], protocol)
+	} else {
+		mappings, err = parseArgs(os.Args[2:], protocol)
+	}
+
 	if err != nil {
 		printUsage()
 		logger.Fatal(err)
@@ -172,23 +247,47 @@ func main() {
 	addedMappings := []Mapping{}
 	publicIP, _ := client.GetExternalIPAddress()
 
-	printLogo()
-	fmt.Println("Currently Forwarding Ports:")
+	if protocol == "clean" {
+		realProtocol := strings.ToUpper(os.Args[2:][0])
+		if realProtocol == "TCP" || realProtocol == "UDP" {
+			for _, m := range mappings {
+				err := client.DeletePortMapping("", uint16(m.RemotePort), realProtocol)
+				if err != nil {
+					logger.Warnf("Failed to remove port mapping %d (%s): %v", m.RemotePort, realProtocol, err)
+				} else {
+					logger.Infof("Removed port mapping %d (%s)", m.RemotePort, realProtocol)
+				}
+			}
+			logger.Info("All specified port mappings removed.")
+			os.Exit(0)
+		} else {
+			printUsage()
+			os.Exit(1)
+		}
+	}
 
 	for _, m := range mappings {
 		desc := fmt.Sprintf("PortUP %s %d", strings.ToUpper(protocol), m.LocalPort)
 		err := client.AddPortMapping("", uint16(m.RemotePort), m.Protocol, uint16(m.LocalPort), m.localIP, true, desc, 0)
 		if err != nil {
-			logger.Fatalf("Failed to add port mapping %d -> %d (%s): %v", m.RemotePort, m.LocalPort, m.Protocol, err)
+			logger.Warnf("Failed to add port mapping %d -> %d (%s): %v", m.RemotePort, m.LocalPort, m.Protocol, err)
+			time.Sleep(50 * time.Millisecond)
+			client = devices[0]
+			continue
 		}
 
+		addedMappings = append(addedMappings, m)
+	}
+
+	printLogo()
+	fmt.Println("Currently Forwarding Ports:")
+
+	for _, m := range addedMappings {
 		fmt.Printf(" %s%s%s:%s%d%s %s->%s %s%s%s:%s%d%s\n",
 			purple, publicIP, reset, cyan, m.RemotePort, reset,
 			green, reset,
 			purple, m.localIP, reset, cyan, m.LocalPort, reset,
 		)
-
-		addedMappings = append(addedMappings, m)
 	}
 
 	sigs := make(chan os.Signal, 1)
@@ -199,7 +298,9 @@ func main() {
 	for _, m := range addedMappings {
 		err := client.DeletePortMapping("", uint16(m.RemotePort), m.Protocol)
 		if err != nil {
-			logger.Printf("Failed to remove port mapping %d (%s): %v", m.RemotePort, m.Protocol, err)
+			logger.Warnf("Failed to remove port mapping %d (%s): %v", m.RemotePort, m.Protocol, err)
+			time.Sleep(50 * time.Millisecond)
+			client = devices[0]
 		}
 	}
 	logger.Info("Shutdown complete.")
